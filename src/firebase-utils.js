20b1e1011dc4915139903c73aa0006be16b4fb2d
teramotodaiki <i101323@gmail.com>
2017-10-03 16:57:37 +0900

    init firebase and make .env

---
+++ src/firebase-utils.js
@@ -0,0 +1,40 @@
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+
+// Initialize Firebase
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: '/auth',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+export function renderWidget(id) {
+  // The start method will wait until the DOM is loaded.
+  ui.start(`#${id}`, uiConfig);
+}
+
+export { firebase, firebaseui };
