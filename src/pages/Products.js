5d2f30ed9f98067afd8a3278d544495f39cae021
teramotodaiki <i101323@gmail.com>
2017-10-04 21:58:05 +0900

    products page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 
-const style = {};
+import { firebase } from '../firebase-utils';
+
+const style = {
+  item: {
+    textAlign: 'center'
+  }
+};
 
 class Products extends Component {
   static propTypes = {
     classes: PropTypes.object.isRequired
   };
 
+  state = {
+    products: []
+  };
+
+  async componentDidMount() {
+    try {
+      const db = firebase.database();
+      const snapshot = await db.ref('/products').once('value');
+      this.setState({ products: snapshot.val() });
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  }
+
   render() {
     const { classes } = this.props;
-    return <div>products</div>;
+    const { products } = this.state;
+
+    return products.map((product, i) => (
+      <div key={i} className={classes.item}>
+        {JSON.stringify(product)}
+      </div>
+    ));
   }
 }
 
