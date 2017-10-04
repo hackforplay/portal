82d3f0af1c07c566d7905625e9751767e0832877
teramotodaiki <i101323@gmail.com>
2017-10-04 20:26:38 +0900

    fetch user from realtime database

--- src/User.js
+++ src/User.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 
+import { firebase } from './firebase-utils';
+
 const style = {};
 
 class User extends Component {
@@ -10,9 +12,27 @@ class User extends Component {
     location: PropTypes.object.isRequired
   };
 
+  state = {
+    user: null
+  };
+
+  async componentDidMount() {
+    const { match: { params } } = this.props;
+    const db = firebase.database();
+    try {
+      const snapshot = await db.ref(`/users/${params.user}`).once('value');
+      this.setState({ user: snapshot.val() });
+    } catch (error) {
+      console.log('Error getting documents: ', error);
+    }
+  }
+
   render() {
-    const { classes, match: { params } } = this.props;
-    return <div>User page of {params.user}</div>;
+    const { classes } = this.props;
+    const { user } = this.state;
+    if (!user) return null;
+
+    return <div>User page of {user.display_name}</div>;
   }
 }
 
