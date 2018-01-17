1e54e358f5cc45ef54ddcdc1dfaeb117fb8215f2
teramotodaiki <i101323@gmail.com>
2018-01-17 17:13:31 +0900

    upgrade material-ui

--- src/settings/theme.js
+++ src/settings/theme.js
@@ -1,26 +1,11 @@
 import { createMuiTheme } from 'material-ui/styles';
 import { orange } from 'material-ui/colors';
-import { dark, light } from 'material-ui/styles/createPalette';
-import { getContrastRatio } from 'material-ui/styles/colorManipulator';
 
 const theme = createMuiTheme({
   palette: {
     primary: orange,
-    // 標準のアルゴリズムでは orange のコントラストカラーは black になってしまうので
-    // 計算部分をオーバーライドする
-    // https://github.com/mui-org/material-ui/blob/v1-beta/src/styles/createPalette.js
-    getContrastText(background) {
-      const contrastThreshold = /* 3.2 */ 2.1;
-      // Use the same logic than
-      // Bootstrap: https://github.com/twbs/bootstrap/blob/1d6e3710dd447de1a200f29e8fa521f8a0908f70/scss/_functions.scss#L59
-      // and material-components-web https://github.com/material-components/material-components-web/blob/ac46b8863c4dab9fc22c4c662dc6bd1b65dd652f/packages/mdc-theme/_functions.scss#L54
-      const contrastText =
-        getContrastRatio(background, dark.text.primary) >= contrastThreshold
-          ? dark.text.primary
-          : light.text.primary;
-
-      return contrastText;
-    }
+    // 標準のアルゴリズムでは orange のコントラストカラーは black になってしまうので閾値を変える
+    contrastThreshold: 2.1
   },
   overrides: {
     MuiCardHeader: {
