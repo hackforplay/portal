63f5cf3c93d6d468ce93864d1f10e4422b43b7cc
teramotodaiki <i101323@gmail.com>
2018-01-26 13:30:21 +0900

    change theme color

--- src/settings/theme.js
+++ src/settings/theme.js
@@ -1,11 +1,9 @@
 import { createMuiTheme } from 'material-ui/styles';
-import { orange } from 'material-ui/colors';
+import { deepOrange } from 'material-ui/colors';
 
 const theme = createMuiTheme({
   palette: {
-    primary: orange,
-    // 標準のアルゴリズムでは orange のコントラストカラーは black になってしまうので閾値を変える
-    contrastThreshold: 2.1
+    primary: deepOrange
   },
   overrides: {
     MuiCardHeader: {
