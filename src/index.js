f01669624d9f2d1317c662e94d96a950226b4926
teramotodaiki <i101323@gmail.com>
2018-01-28 17:30:43 +0900

    install moment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import moment from 'moment';
+import 'moment/locale/ja';
 import 'normalize.css';
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './ducks';
+
+moment.locale(navigator.language);
 
 const middleware = [thunk];
 if (process.env.NODE_ENV !== 'production') {
