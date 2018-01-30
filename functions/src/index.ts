570c00d23cfd4201e4703585df468f62afd740e5
teramotodaiki <i101323@gmail.com>
2018-01-31 08:49:37 +0900

    add user document when authenticated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const users = admin.firestore().collection('users');
+
+type UserDocumentData = {
+  uid: string;
+  displayName: string;
+  email: string;
+  photoURL: string;
+  worksNum: number;
+  createdAt: string;
+};
+
+export const addUserDocument = functions.auth.user().onCreate(event => {
+  // ユーザーが初めて OAuth 認証を行った時、Firestore に新しいドキュメントを追加する
+  const user = event.data;
+
+  const data: UserDocumentData = {
+    uid: user.uid,
+    displayName: user.displayName,
+    email: user.email, // いずれ非公開情報にする
+    photoURL: user.photoURL,
+    worksNum: 0,
+    createdAt: event.timestamp
+  };
+
+  return users.doc(user.uid).create(data);
+});
