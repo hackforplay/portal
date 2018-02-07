fcbb32c63c3979bbcbcdfbe2816a5721ae08a8bf
teramotodaiki <i101323@gmail.com>
2018-02-07 13:41:46 +0900

    remove: Tutorials

--- src/containers/Tutorials.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Tutorials from '../components/Tutorials';
-
-const mapStateToProps = (state, ownProps) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Tutorials);
