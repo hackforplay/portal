10fc80ae3fbdba4a4efe4ee20ddb9e9c303b4722
teramotodaiki <i101323@gmail.com>
2018-02-08 22:05:44 +0900

    feature: ranking view

--- src/components/PCRanking.jsx
+++ src/components/PCRanking.jsx
@@ -1,26 +1,77 @@
+// @flow
 import * as React from 'react';
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+import { CircularProgress } from 'material-ui/Progress';
+import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 
+import theme from '../settings/theme';
 import type { RecordCollectionType } from '../ducks/pcRanking';
 
 type Props = {
-  classes: {},
+  classes: {
+    root: string,
+    progress: string
+  },
   records: RecordCollectionType
 };
 
-const stages = ['stages/semi1', 'stages/semi2', 'stages/semi3'];
-
-@withStyles({})
+@withStyles({
+  root: {
+    maxWidth: 840,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  progress: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: theme.spacing.unit * 6
+  }
+})
 export default class PCRanking extends React.Component<Props> {
   render() {
     const { classes, records } = this.props;
 
     return (
-      <div>
+      <div className={classes.root}>
         <h1>ランキング</h1>
-        {records.isAvailable
-          ? records.data.map(item => <div key={item.id}>{item.name}</div>)
-          : 'Loading'}
+        {records.isAvailable ? (
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>順位</TableCell>
+                  <TableCell>名前</TableCell>
+                  <TableCell>スコア</TableCell>
+                  <TableCell>残り時間</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {records.data.map((item, i) => (
+                  <TableRow key={item.id}>
+                    <TableCell>{`${i + 1}位`}</TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{item.score}</TableCell>
+                    <TableCell>{`${item.lastTime}秒`}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        ) : records.isEmpty ? (
+          <Typography type="display1">
+            まだ記録がないか、無効なステージです :-(
+          </Typography>
+        ) : (
+          <CircularProgress className={classes.progress} />
+        )}
       </div>
     );
   }
