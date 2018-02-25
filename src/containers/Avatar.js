1777ffc2f23a781e72a6a4a8f39743cb4122c0cd
teramotodaiki <i101323@gmail.com>
2018-02-26 08:44:51 +0900

    refactor: make decorator

---
+++ src/containers/Avatar.js
@@ -0,0 +1,5 @@
+import Avatar from 'material-ui/Avatar';
+
+import withFirebaseStorage from '../decorators/withFirebaseStorage';
+
+export default withFirebaseStorage(Avatar);
---
+++ src/decorators/withFirebaseStorage.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+import { StoreState } from '../ducks';
+import { downloadUrl, getStorageByPath } from '../ducks/storage';
+
+const mapStateToProps = (state: StoreState, ownProps) => {
+  const storage = getStorageByPath(state, ownProps.storagePath);
+
+  return ownProps.storagePath
+    ? {
+        src: storage.url || ' '
+      }
+    : {};
+};
+
+const mapDispatchToProps = {
+  downloadUrl
+};
+
+export default function withFirebaseStorage(WrappedComponent: React.Component) {
+  @connect(mapStateToProps, mapDispatchToProps)
+  class Component extends React.Component {
+    componentDidMount() {
+      if (this.props.storagePath) {
+        this.props.downloadUrl(this.props.storagePath);
+      }
+    }
+
+    componentDidUpdate(prevProps) {
+      if (
+        prevProps.storagePath !== this.props.storagePath &&
+        this.props.storagePath
+      ) {
+        this.props.downloadUrl(this.props.storagePath);
+      }
+    }
+
+    render() {
+      // 余分な props を削除する
+      const { storagePath, downloadUrl, src, ...props } = this.props;
+      if (src) {
+        // イメージ
+        return <WrappedComponent {...props} src={src} />;
+      }
+      if (props.alt) {
+        // alt を表示
+        return <WrappedComponent {...props}>{props.alt}</WrappedComponent>;
+      }
+      // そのまま描画
+      return <WrappedComponent {...props} />;
+    }
+  }
+  return Component;
+}
