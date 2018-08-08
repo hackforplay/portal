8fef6252ee9b4cd78e301f21eab355429cc95978
teramotodaiki <i101323@gmail.com>
2018-08-08 22:58:11 +0900

    feature: make SFC

--- src/components/OptionalHeader.jsx
+++ src/components/OptionalHeader.jsx
@@ -1,53 +1,49 @@
 // @flow
 import * as React from 'react';
+import { compose } from 'redux';
 import pathToRegexp from 'path-to-regexp';
 import { withRouter } from 'react-router-dom';
 import type { ContextRouter } from 'react-router-dom';
 import Typography from 'material-ui/Typography';
-import { withStyles } from 'material-ui/styles';
 import { grey } from 'material-ui/colors';
+import { css } from 'emotion';
 
 import theme from '../settings/theme';
 import { optionalHeaderInfo } from '../settings/siteMap';
 
-type Props = {
-  classes: {}
-} & ContextRouter;
-
-@withRouter
-@withStyles({
-  root: {
+const classes = {
+  root: css({
     paddingTop: theme.spacing.unit * 6,
     paddingBottom: theme.spacing.unit * 6,
     backgroundColor: grey[300]
+  })
+};
+
+export type OwnProps = {};
+type Props = OwnProps & { ...ContextRouter };
+
+export default compose(withRouter)((props: Props) => {
+  const { location } = props;
+
+  // 現在表示している URL にふさわしいタブの状態を取得する
+  const info = optionalHeaderInfo.find(item => {
+    const re = pathToRegexp(item.path);
+    return re.exec(location.pathname);
+  });
+
+  if (!info) {
+    // TODO: アニメーション
+    return null;
   }
-})
-class OptionalHeader extends React.Component<Props> {
-  render() {
-    const { classes, location } = this.props;
 
-    // 現在表示している URL にふさわしいタブの状態を取得する
-    const info = optionalHeaderInfo.find(item => {
-      const re = pathToRegexp(item.path);
-      return re.exec(location.pathname);
-    });
-
-    if (!info) {
-      // TODO: アニメーション
-      return null;
-    }
-
-    return (
-      <div className={classes.root}>
-        <Typography align="center" type="headline" gutterBottom>
-          {info.headline}
-        </Typography>
-        <Typography align="center" type="caption">
-          {info.caption}
-        </Typography>
-      </div>
-    );
-  }
-}
-
-export default OptionalHeader;
+  return (
+    <div className={classes.root}>
+      <Typography align="center" type="headline" gutterBottom>
+        {info.headline}
+      </Typography>
+      <Typography align="center" type="caption">
+        {info.caption}
+      </Typography>
+    </div>
+  );
+});
