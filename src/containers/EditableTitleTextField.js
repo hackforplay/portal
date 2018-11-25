7fc735f8c25d7180e938b449a4b0c1b8eac77fb2
teramotodaiki <i101323@gmail.com>
2018-11-25 18:24:31 +0900

    feature: タイトルを更新したら自動でセーブする

--- src/containers/EditableTitleTextField.js
+++ src/containers/EditableTitleTextField.js
@@ -1,21 +1,30 @@
 // @flow
 import { connect } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
+import debounce from 'debounce';
 
-import { actions } from '../ducks/make';
+import { actions, saveWork } from '../ducks/make';
 import type { StoreState } from '../ducks/';
 
 const mapStateToProps = (state: StoreState, ownProps) => ({
   value: state.make.metadata.title || ''
 });
 
+// １秒間操作がなければ saveWork
+const save = debounce(dispatch => dispatch(saveWork()), 1000);
+
 const mapDispatchToProps = {
-  onChange: event => dispatch =>
+  onChange: event => dispatch => {
     dispatch(
       actions.metadata({
         title: event.target.value
       })
-    )
+    );
+    save(dispatch);
+  }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(TextField);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TextField);
