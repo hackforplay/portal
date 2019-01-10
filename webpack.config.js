e61b68a70289a4e01ade7f9006d89efb4c796fae
teramotodaiki <i101323@gmail.com>
2019-01-10 13:13:22 +0900

    dev: dotenv をロード

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const webpack = require('webpack');
+const fs = require('fs');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: ['@babel/polyfill', 'whatwg-fetch', './src/index.js'],
@@ -51,7 +55,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       template: path.resolve(__dirname, './public/index.html')
-    })
+    }),
+    new webpack.DefinePlugin(getClientEnvironment())
   ],
   devServer: {
     contentBase: path.join(__dirname, 'build'),
@@ -59,7 +64,78 @@ module.exports = {
     port: 9000
   },
   devtool:
-    process.env.NODE_ENV === 'production'
-      ? 'source-map'
-      : 'cheap-module-eval-source-map'
+    NODE_ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map'
 };
+
+function getClientEnvironment() {
+  // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
+  const dotenvFiles = [
+    `.env.${NODE_ENV}.local`,
+    `.env.${NODE_ENV}`,
+    // Don't include `.env.local` for `test` environment
+    // since normally you expect tests to produce the same
+    // results for everyone
+    NODE_ENV !== 'test' && `.env.local`,
+    '.env'
+  ].filter(Boolean);
+
+  // Load environment variables from .env* files. Suppress warnings using silent
+  // if this file is missing. dotenv will never modify any environment variables
+  // that have already been set.
+  // https://github.com/motdotla/dotenv
+  dotenvFiles.forEach(dotenvFile => {
+    if (fs.existsSync(dotenvFile)) {
+      require('dotenv').config({
+        path: dotenvFile
+      });
+    }
+  });
+
+  // We support resolving modules according to `NODE_PATH`.
+  // This lets you use absolute paths in imports inside large monorepos:
+  // https://github.com/facebookincubator/create-react-app/issues/253.
+  // It works similar to `NODE_PATH` in Node itself:
+  // https://nodejs.org/api/modules.html#modules_loading_from_the_global_folders
+  // Note that unlike in Node, only *relative* paths from `NODE_PATH` are honored.
+  // Otherwise, we risk importing Node.js core modules into an app instead of Webpack shims.
+  // https://github.com/facebookincubator/create-react-app/issues/1023#issuecomment-265344421
+  // We also resolve them to make sure all tools using them work consistently.
+  const appDirectory = fs.realpathSync(process.cwd());
+  process.env.NODE_PATH = (process.env.NODE_PATH || '')
+    .split(path.delimiter)
+    .filter(folder => folder && !path.isAbsolute(folder))
+    .map(folder => path.resolve(appDirectory, folder))
+    .join(path.delimiter);
+
+  // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+  // injected into the application via DefinePlugin in Webpack configuration.
+  const REACT_APP = /^REACT_APP_/i;
+
+  const raw = Object.keys(process.env)
+    .filter(key => REACT_APP.test(key))
+    .reduce(
+      (env, key) => {
+        env[key] = process.env[key];
+        return env;
+      },
+      {
+        // Useful for determining whether we’re running in production mode.
+        // Most importantly, it switches React into the correct mode.
+        NODE_ENV,
+        // Useful for resolving the correct path to static assets in `public`.
+        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
+        // This should only be used as an escape hatch. Normally you would put
+        // images into the `src` and `import` them in code to get their paths.
+        PUBLIC_URL: ''
+      }
+    );
+  // Stringify all values so we can feed into Webpack DefinePlugin
+  const stringified = {
+    'process.env': Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {})
+  };
+
+  return stringified;
+}
